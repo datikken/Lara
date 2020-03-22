ef9b88407bc6dcb9722c1e9e61664a8f43990f24
tikken <tikken23@gmail.com>
2020-03-22 11:44:51 +0300

    add item to cart

---
+++ app/Cart.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: tikken
+ * Date: 3/22/20
+ * Time: 11:28 AM
+ */
+
+namespace App;
+
+
+class Cart
+{
+    public $items;
+    public $totalQuantity;
+    public $totalPrice;
+
+    public function __construct($prevCart)
+    {
+        if($prevCart != null)
+        {
+            $this->items = $prevCart->items;
+            $this->totalQuantity = $prevCart->totalQuantity;
+            $this->totalPrice = $prevCart->totalPrice;
+        } else {
+            $this->items = [];
+            $this->totalQuantity = 0;
+            $this->totalPrice = 0;
+        }
+    }
+
+    public function addItem($id, $product)
+    {
+        $price = (int) str_replace('$', '', $product->price);
+        //check if item already in cart
+        if(array_key_exists($id, $this->items))
+        {
+            $productToAdd = $this->items[$id];
+            $productToAdd['quantity']++;
+
+        } else {
+            $productToAdd = ['quantity' => 1, 'price' => $price, 'data'=> $product];
+
+        }
+
+        $this->items[$id] = $productToAdd;
+        $this->totalQuantity++;
+        $this->totalPrice = $this->totalPrice + $price;
+    }
+}
