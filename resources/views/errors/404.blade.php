4813ac27efc5161e65e7d22f957da1423a7fd31c
tikken <tikken23@gmail.com>
2020-03-22 14:54:37 +0300

    fix

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,9 +1,24 @@
 
-<link rel="stylesheet" href="/css/app.css">
-@extends('layouts.index')
 
-@section('center')
-    <div class="error404">
-        <img src='/images/404/404.svg' alt="Error" />
-    </div>
-@endsection
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    <script src="{!! mix('js/app.js') !!}"></script>
+    <link rel="stylesheet" href="/css/app.css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+</head>
+<body>
+    @extends('layouts.index')
+
+    @section('center')
+        <div class="error404">
+            <img src='/images/404/404.svg' alt="Error" />
+        </div>
+    @endsection
+</body>
+</html>
+
+
