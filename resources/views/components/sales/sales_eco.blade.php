21ef85372bdd0f6f677bc2c0b3b66c267859418c
datikken <datikken@datikkens-Air.Dlink>
2020-03-26 00:14:19 +0300

    more mobile styles

--- resources/views/components/sales/sales_eco.blade.php
+++ resources/views/components/sales/sales_eco.blade.php
@@ -1,5 +1,17 @@
 <div class="sales_eco">
     <div class="sales_eco-wrapper">
-        <img src='/images/sales/sales_eco.png' alt="images"/>
+        <div class="sales_eco-wrapper_item">
+            <div class="sales_eco-wrapper_item-head">
+                <p>Баннер</p>
+            </div>
+            <div class="sales_eco-wrapper_item-desc">
+                <p>Рассказывающий про программу эко</p>
+            </div>
+        </div>
+        <div class="sales_eco-wrapper_item">
+            <div class="sales_eco-wrapper_item-btn">
+                @include('components.btn.text_btn', [ 'class' => '', 'text' => 'подробнее'])
+            </div>
+        </div>
     </div>
 </div>
