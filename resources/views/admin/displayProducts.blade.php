802caf9673d8623a56466d2d34b14f05ec3b2802
tikken <tikken23@gmail.com>
2020-04-04 19:02:53 +0300

    test

--- resources/views/admin/displayProducts.blade.php
+++ resources/views/admin/displayProducts.blade.php
@@ -7,17 +7,28 @@
             <thead>
             <tr>
                 <th>#</th>
-                <th>Name</th>
-                <th>Image</th>
+                <th>Имя</th>
+                <th>Цена</th>
+                <th>Тип</th>
+                <th>Описание</th>
+                <th>Изображение</th>
+                <th>Создан</th>
+                <th>Обновлен</th>
             </tr>
             </thead>
             <tbody>
 
             @foreach($products as $product)
             <tr>
+
                 <td>{{$product['id']}}</td>
                 <td>{{$product['name']}}</td>
+                <td>{{$product['price']}}</td>
+                <td>{{$product['type']}}</td>
+                <td>{{$product['description']}}</td>
                 <td>{{$product['image']}}</td>
+                <td>{{$product['created_at']}}</td>
+                <td>{{$product['updated_up']}}</td>
 
                 <td>
                     {{--<a href="{{route('admin.products.editImageForm', ['id' => $product['id']]) }}" class="btn btn-primary">Edit image</a>--}}
