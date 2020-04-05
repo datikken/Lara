474f6c79b34d070ab574cf0c73fa184e9e957ccb
tikken <tikken23@gmail.com>
2020-04-05 17:45:30 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('pages.index');
-});
+Route::get('/',['uses' => 'ProductsController@index', 'as' => 'allProducts']);
 
 //Catalog
 Route::get('catalog', ['uses' => "ProductsController@index", 'as' => 'allProducts']);
