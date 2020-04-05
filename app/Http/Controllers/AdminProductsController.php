391cf824a0afc4fcac3dca24edecaab60e719646
tikken <tikken23@gmail.com>
2020-04-05 14:52:01 +0300

    facade fix

--- app/Http/Controllers/AdminProductsController.php
+++ app/Http/Controllers/AdminProductsController.php
@@ -8,7 +8,7 @@ use App\Product;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\facades\File;
+use Illuminate\Support\Facades\File;
 
 class AdminProductsController extends Controller
 {
