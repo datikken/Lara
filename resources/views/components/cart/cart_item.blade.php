44d20a0beb187b9c2075aae3a7249ef7706f5413
tikken <tikken23@gmail.com>
2020-04-05 20:24:24 +0300

    decrease increase cart items

--- resources/views/components/cart/cart_item.blade.php
+++ resources/views/components/cart/cart_item.blade.php
@@ -46,15 +46,17 @@
                 </div>
 
                 <div class="cart_wrap-item_inner-table_row-col_btns-btn-items">
-                    <div class="cart_wrap-item_inner-table_row-col_btns-btn-items_minus">
-                        <a href="{{ route('DecreaseSingleProduct', ['id' => $cartItem['data']['id']])}}">-</a>
-                    </div>
+                    <a href="{{ route('DecreaseSingleProduct', ['id' => $cartItem['data']['id']])}}">
+                        <div class="cart_wrap-item_inner-table_row-col_btns-btn-items_minus">-</div>
+                    </a>
+
                     <div class="cart_wrap-item_inner-table_row-col_btns-btn-items_quantity">
                         <span>{{$cartItem['quantity']}}</span>
                     </div>
-                    <div class="cart_wrap-item_inner-table_row-col_btns-btn-items_plus">
-                        <a href="{{ route('IncreaseSingleProduct', ['id' => $cartItem['data']['id']])}}">+</a>
-                    </div>
+
+                    <a href="{{ route('IncreaseSingleProduct', ['id' => $cartItem['data']['id']])}}">
+                        <div class="cart_wrap-item_inner-table_row-col_btns-btn-items_plus">+</div>
+                    </a>
                 </div>
 
             </div>
