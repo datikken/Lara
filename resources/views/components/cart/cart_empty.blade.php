532a59bbd292f3b2ceb82547253c32004500ef51
tikken <tikken23@gmail.com>
2020-04-07 02:09:12 +0300

    empty cart

--- resources/views/components/cart/cart_empty.blade.php
+++ resources/views/components/cart/cart_empty.blade.php
@@ -1,5 +1,8 @@
 <?php #dump($cartItem)?>
 
 <div class="cart_wrap-item_inner-table_row">
-    <h2 style="margin: 0 auto;">Здесь пока пусто!</h2>
+    <h2>Ваша корзина еще пуста</h2>
+    <p>Для удобства наполнения корзины, Вы можете воспользоваться поиском
+        или каталогом товаров</p>
+    @include('components.btn.text_btn', [ 'class' => 'flat_btn', 'text' => 'перейти в каталог'])
 </div>
