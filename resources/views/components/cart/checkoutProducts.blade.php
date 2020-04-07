0168d77ce3fb9ccbc9d8eebb34820a0f61e60de7
tikken <tikken23@gmail.com>
2020-04-08 02:46:45 +0300

    checkout form

--- resources/views/components/cart/checkoutProducts.blade.php
+++ resources/views/components/cart/checkoutProducts.blade.php
@@ -7,19 +7,22 @@
 
         <div class="cart_check-wrap_item">
             <div class="cart_check-wrap_item-group">
-                <label for="name">Имя</label>
+                <label for="name">Имя *</label>
                 <input type="text" name="name" />
+                <label for="required">Поле имя обязательно к заполнению</label>
             </div>
             <div class="cart_check-wrap_item-group">
-                <label for="lastname">Фамилия</label>
+                <label for="lastname">Фамилия *</label>
                 <input type="text" name="lastname" />
+                <label for="required">Поле фамилия обязательно к заполнению</label>
             </div>
         </div>
 
         <div class="cart_check-wrap_item">
             <div class="cart_check-wrap_item-group">
-                <label for="name">Телефон</label>
-                <input type="text" name="name" />
+                <label for="name">Телефон *</label>
+                <input type="text" name="name" placeholder="+7 (___) ___ - __ - __"/>
+                <label for="required">Поле телефон обязательно к заполнению</label>
             </div>
             <div class="cart_check-wrap_item-group">
                 @include('components.btn.text_btn', [ 'class' => 'cart_check-wrap_item-group_btn', 'text' => 'продолжить'])
@@ -27,9 +30,9 @@
         </div>
 
         <div class="cart_check-wrap_item">
-            <div class="cart_check-wrap_item-group">
-                <input type="text" name="name" />
-                <label for="name">Сохранить данные</label>
+            <div class="cart_check-wrap_item-group checkbox">
+                <input type="checkbox" name="save" />
+                <label for="save">Сохранить данные</label>
             </div>
         </div>
 
