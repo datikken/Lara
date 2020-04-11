ea51cde887e32e82f7c4d8842045e8bab07bb6c3
tikken <tikken23@gmail.com>
2020-04-11 15:03:41 +0300

    small fixes

--- resources/views/pages/cart/deliveryForm.blade.php
+++ resources/views/pages/cart/deliveryForm.blade.php
@@ -88,15 +88,39 @@
 
                 <form class="metro_form" method="POST" action="{{ route('setIssue') }}">
                     <div class="metro_group">
-                        <div class="form_group">
+                        <div class="form_group metro_group_wrap">
                             <label for="metro" class="form_group-label">Выбрать ближайшее метро</label>
-                            <input type="text" name="metro">
+                            <input type="text" name="metro" value="" class="invisible" />
+
+                            <div class="metro_group_wrap_list-wrap">
+                                <span class="metro_group_wrap_list-wrap-heading">Введите ближайшее метро</span>
+                                <div class="metro_group_wrap_list-wrap-icon" style="background-image: url('/images/icons/arrow_right.svg')"></div>
+                                <ul>
+                                    @foreach (Config::get('constants.STATIONS') as $station)
+                                        <li>{{ $station }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
                         </div>
 
                         <div class="form_group">
                             <label for="station" class="form_group-label">Выбрать пункт выдачи</label>
                             <input type="text" name="station">
+
+                            <div class="postamat_list-wrap">
+                                <span class="postamat_list-wrap-heading">Введите ближайшее метро</span>
+                                <div class="postamat_list-wrap-icon" style="background-image: url('/images/icons/arrow_right.svg')"></div>
+                                <ul>
+                                    @foreach (Config::get('constants.POSTAMATS') as $station)
+                                        <li>{{ $station }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+
                         </div>
+
+
+
                     </div>
                     @include('components.btn.text_btn', [ 'class' => 'form_group-btn flat_btn', 'text' => 'Забрать из этого пункта'])
                 </form>
