bac97d9c520e2aa643c2bd12ed73ba3a6ebfe7b8
tikken <tikken23@gmail.com>
2020-04-20 22:23:18 +0300

    mail config

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'tikken23@deonis39.ru'),
+        'name' => env('MAIL_FROM_NAME', 'tikken23'),
     ],
 
     /*
