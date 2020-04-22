9ccdf0fb208f28e3bea8365e9d4410f5f336dc32
tikken <tikken23@gmail.com>
2020-04-22 20:33:57 +0300

    fix

--- resources/views/pages/dash/dash_fill.blade.php
+++ resources/views/pages/dash/dash_fill.blade.php
@@ -2,6 +2,7 @@
 
 @section('center')
    <div class="dfill">
+
        <div class="dfill_wrap">
            <div class="dfill_wrap-heading">
                 <span>Для дальнейшего удобства заказа, рекомендуем вам заполнить аккаунт. Эти данные потребуются при формировании корзины.</span>
@@ -12,27 +13,56 @@
                     <div class="dfill_wrap-form_top-left_inner" style="background-image: url('/images/dash/dash_avatar.svg')"></div>
                 </div>
                 <div class="dfill_wrap-form_top-right">
-                    <form action="/" action="post">
+                    <form action="/" method="post">
                         {{csrf_field()}}
 
                         <div class="form-group">
                             <label for="name">Имя <span>*</span></label>
-                            <input type="text" class="form-control" name="name" placeholder="Имя" required>
+                            <div class="input_wrap">
+                                <input type="text" class="form-control" name="name" placeholder="Имя" required>
+                                <button class="form-group-btn">
+                                    <span>
+                                        изменить
+                                    </span>
+                                </button>
+                            </div>
                         </div>
 
                         <div class="form-group">
                             <label for="name">Фамилия <span>*</span></label>
-                            <input type="text" class="form-control" name="lastname" placeholder="Фамилия" required>
+                            <div class="input_wrap">
+                                <input type="text" class="form-control" name="lastname" placeholder="Фамилия" required>
+                                <button class="form-group-btn">
+                                     <span>
+                                        изменить
+                                    </span>
+                                </button>
+                            </div>
                         </div>
 
                         <div class="form-group">
                             <label for="name">Телефон <span>*</span></label>
-                            <input type="text" class="form-control" name="tel" placeholder="Телефон" required>
+
+                            <div class="input_wrap">
+                                <input type="text" class="form-control" name="tel" placeholder="Телефон" required>
+                                <button class="form-group-btn">
+                                    <span>
+                                        изменить
+                                    </span>
+                                </button>
+                            </div>
                         </div>
 
                         <div class="form-group">
                             <label for="name">Адрес электронной почты <span>*</span></label>
-                            <input type="text" class="form-control" name="email" placeholder="Адрес электронной почты" required>
+                            <div class="input_wrap">
+                                <input type="text" class="form-control" name="email" placeholder="Адрес электронной почты" required>
+                                <button class="form-group-btn">
+                                    <span>
+                                        изменить
+                                    </span>
+                                </button>
+                            </div>
                         </div>
 
                         {{--<button type="submit" name="submit" class="btn btn-primary">Submit</button>--}}
@@ -40,8 +70,39 @@
                     </form>
                 </div>
             </div>
-
        </div>
+
+        <div class="dchange">
+            <form action="/" method="post">
+                {{csrf_field()}}
+
+                <div class="form-group">
+                    <label for="name">Действуйщий пароль <span>*</span></label>
+                    <input type="text" class="form-control" name="password" placeholder="Имя" required>
+                </div>
+
+                <div class="form-group">
+                    <label for="name">Новый пароль <span>*</span></label>
+                    <input type="text" class="form-control" name="newpassword" placeholder="Имя" required>
+                </div>
+
+                <div class="form-group">
+                    <label for="name">Подтвердите пароль <span>*</span></label>
+                    <input type="text" class="form-control" name="dpassword" placeholder="Имя" required>
+                </div>
+
+                <button>
+                    @include('components.btn.text_btn', [ 'class' => 'dchange_btn', 'text' => 'Сохранить изменения'])
+                </button>
+
+            </form>
+
+            <div class="dchange_remind">
+               <p>Поля отмеченые <span>*</span> обязательны к заполнению</p>
+            </div>
+
+
+        </div>
    </div>
 
 @endsection
