661b8ef4c201581402db1e8a32ee25f1b7859d1e
tikken <tikken23@gmail.com>
2020-05-01 08:36:44 +0300

    test

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -14,7 +14,14 @@ class AboutController
     public function index()
     {
 
-        $years = Config::get('constants.ABOUTYEARS');
+        $years = [
+            2009, 2010,
+            2011, 2012,
+            2013, 2014,
+            2015, 2016,
+            2017, 2018,
+            2019, 2020
+        ];
 
         return view('pages.about', ['years' => $years]);
     }
