87a400e7c48cf0d5e9f5c0c4e713df4b5e468331
tikken <tikken23@gmail.com>
2020-05-14 01:09:02 +0300

    fixes

--- resources/views/admin/about/display.blade.php
+++ resources/views/admin/about/display.blade.php
@@ -15,6 +15,7 @@
                 <th>Создан</th>
                 <th>Обновлен</th>
                 <th>Удалить</th>
+                <th>Создать</th>
             </tr>
             </thead>
             <tbody>
@@ -31,6 +32,14 @@
                     <td>
                         <a href="{{ route('AdminDeleteAbout', ['id' => $year['id']]) }}" class="btn btn-danger">
                             <span class="ui-icon uk-icon" uk-icon="close"><svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-svg="close"><path fill="none" stroke="#000" stroke-width="1.06" d="M16,16 L4,4"></path><path fill="none" stroke="#000" stroke-width="1.06" d="M16,4 L4,16"></path></svg></span>
+                        </a>
+                    </td>
+                    <td>
+                        <a href="{{ route('adminDisplayCreateAbout') }}" class="btn btn-primary">
+                            <svg class="bi bi-plus" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M8 3.5a.5.5 0 01.5.5v4a.5.5 0 01-.5.5H4a.5.5 0 010-1h3.5V4a.5.5 0 01.5-.5z" clip-rule="evenodd"></path>
+                                <path fill-rule="evenodd" d="M7.5 8a.5.5 0 01.5-.5h4a.5.5 0 010 1H8.5V12a.5.5 0 01-1 0V8z" clip-rule="evenodd"></path>
+                            </svg>
                         </a>
                     </td>
                 </tr>
