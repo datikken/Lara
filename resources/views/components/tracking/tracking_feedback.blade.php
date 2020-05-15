77f8946881a95efcd093f7a381dafe721d031361
tikken <tikken23@gmail.com>
2020-05-16 00:04:04 +0300

    fixes

---
+++ resources/views/components/tracking/tracking_feedback.blade.php
@@ -0,0 +1,24 @@
+<div class="trackfeed">
+    <div class="trackfeed_wrap">
+        <div class="trackfeed_head">
+            <span>Мы любим, когда товар доставлен надлежащего качества и в назначенный срок. Поэтому, мы будем рады вашему отклику относительно доставки.</span>
+        </div>
+
+        <div class="trackfeed_theme">
+            <div class="trackfeed_feedItem">
+                <span>Пожелания</span>
+            </div>
+            <div class="trackfeed_feedItem">
+                <span>Вопрос</span>
+            </div>
+            <div class="trackfeed_feedItem">
+                <span>Претензия</span>
+            </div>
+        </div>
+
+        <div class="trackfeed_content">
+            <label for="trackfeed" class="trackfeed_label"></label>
+            <textarea name="" id="" class="trackfeed_textarea" cols="30" rows="10"></textarea>
+        </div>
+    </div>
+</div>
--- resources/views/pages/tracking.blade.php
+++ resources/views/pages/tracking.blade.php
@@ -3,4 +3,6 @@
 
     @include('components.breadcrumbs.breadcrumbs', ['crumbs' => ['Главная', 'Профиль', 'Трекинг заказа']])
     @include('components.tracking.tracking_progress')
+    @include('components.tracking.tracking_feedback')
+
 @endsection
