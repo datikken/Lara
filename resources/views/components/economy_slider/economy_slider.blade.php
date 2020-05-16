56833c18b73b8c3ae52d09701e5423ba29f3910b
tikken <tikken23@gmail.com>
2020-05-17 02:15:59 +0300

    econom slider

---
+++ resources/views/components/economy_slider/economy_slider.blade.php
@@ -0,0 +1,44 @@
+<div class="economslider">
+    <div class="economslider_wrap">
+
+        <div class="economslider_slide">
+            <div class="economslider_labels">
+                <div class="economslider_label-left"><span>1 год</span></div>
+                <div class="economslider_label"></div>
+                <div class="economslider_label-right"><span>10 год</span></div>
+            </div>
+
+            <div class="economslider_line"></div>
+        </div>
+
+        <div class="economslider_cards">
+            <div class="economslider_card">
+                <div class="economslider_card_head">
+                    <div class="economslider_image economslider_image-left"></div>
+                </div>
+                <div class="economslider_card_footer">
+                    <span class="economslider_value">79900</span>
+                </div>
+            </div>
+
+            <div class="economslider_card">
+                <div class="economslider_card_head">
+                    <div class="economslider_image  economslider_image-right"></div>
+                </div>
+                <div class="economslider_card_footer">
+                    <span class="economslider_value">29900</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="economslider_footer">
+            <div class="economslider_footer_head">
+                <span>экономия</span>
+            </div>
+            <div class="economslider_footer_btn">
+                <span class="economslider_economy">40000</span>
+            </div>
+        </div>
+
+    </div>
+</div>
