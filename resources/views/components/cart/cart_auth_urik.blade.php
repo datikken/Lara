5b7ab120565c304c190524fd7462a90ec227b841
tikken <tikken23@gmail.com>
2020-05-28 02:03:34 +0300

    fixes

--- resources/views/components/cart/cart_auth_urik.blade.php
+++ resources/views/components/cart/cart_auth_urik.blade.php
@@ -1,21 +1,21 @@
 <div class="company">
     <div class="company_wrap">
         <div class="company_head">
-            <h1>данные компании</h1>
+            <h1 class="company_head_item">данные компании</h1>
         </div>
 
         <div class="company_inputs">
             <div class="company_inputs_items">
-                <label for="inn">ИНН <span>*</span></label>
-                <input type="text" name="inn" placeholder="12345678910">
+                <label class="company_inputs_items_label" for="inn">ИНН <span>*</span></label>
+                <input class="company_input" type="text" name="inn" placeholder="12345678910">
             </div>
             <div class="company_inputs_items">
-                <label for="bik">Бик <span>*</span></label>
-                <input type="text" name="bik" placeholder="123456789">
+                <label class="company_inputs_items_label" for="bik">Бик <span>*</span></label>
+                <input class="company_input" type="text" name="bik" placeholder="123456789">
             </div>
             <div class="company_inputs_items">
-                <label for="schet">Расчетный счет <span>*</span></label>
-                <input type="text" name="schet" placeholder="12345678901234567890">
+                <label class="company_inputs_items_label" for="schet">Расчетный счет <span>*</span></label>
+                <input class="company_input" type="text" name="schet" placeholder="12345678901234567890">
             </div>
         </div>
 
