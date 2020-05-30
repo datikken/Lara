d4727c7a63d7b631d2719d383f36d90813750b52
tikken <tikken23@gmail.com>
2020-05-31 01:23:09 +0300

    test

--- resources/views/pages/tracking_result.blade.php
+++ resources/views/pages/tracking_result.blade.php
@@ -2,9 +2,6 @@
 
 
 @section('center')
-
-    @php dump($order[0]); @endphp
-
     @include('components.breadcrumbs.breadcrumbs', ['crumbs' =>[['title' => 'Главная','url' => '/'],
         ['title' => 'Профиль', 'url' => '/home'],
         ['title' => 'Трекинг заказа','url' => '/tracking']]])
