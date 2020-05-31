f1b0c3b5d14ee0598fffa5b0c2e4ea60299d3c0b
tikken <tikken23@gmail.com>
2020-05-31 20:50:30 +0300

    blog post item

--- resources/views/components/blog/posts_list.blade.php
+++ resources/views/components/blog/posts_list.blade.php
@@ -1,6 +1,6 @@
 <div class="postList">
     @foreach ($posts as $post)
-            <a href="{{ route('postDetails',['id' => $post['id']]) }}" class="postList_item">
+            <div class="postList_item">
                 <div class="postList_cat">
                     <span>{{ $post['category'] }}</span>
                 </div>
@@ -16,36 +16,44 @@
                 </div>
 
                 <div class="postList_btns">
-                    @include('components.btn.text_btn', [ 'class' => '', 'text' => 'Читать далее'])
+                    <div class="postList_btns_inner">
+                        <a href="{{ route('postDetails',['id' => $post['id']]) }}">
+                            @include('components.btn.text_btn', [ 'class' => '', 'text' => 'Читать далее'])
+                        </a>
 
-                    <div class="postList_btns_actions">
-                        <div class="postList_like">
-                            <div class="postList_like_icon"></div>
-                            <div class="postList_like_val">{{ $post['likes'] }}</div>
-                        </div>
+                        <div class="postList_btns_actions">
+                            <div class="postList_like">
+                                <div class="postList_like_icon"></div>
+                                <div class="postList_like_val">{{ $post['likes'] }}</div>
+                            </div>
 
-                        <div class="postList_dislike">
-                            <div class="postList_dislike_icon"></div>
-                            <div class="postList_dislike_val">{{ $post['dislikes'] }}</div>
-                        </div>
+                            <div class="postList_dislike">
+                                <div class="postList_dislike_icon"></div>
+                                <div class="postList_dislike_val">{{ $post['dislikes'] }}</div>
+                            </div>
 
-                        <div class="postList_comment">
-                            <div class="postList_comment_icon"></div>
-                            <div class="postList_comment_val">{{ $post['dislikes'] }}</div>
-                        </div>
+                            <div class="postList_comment">
+                                <div class="postList_comment_icon"></div>
+                                <div class="postList_comment_val">{{ $post['dislikes'] }}</div>
+                            </div>
 
-                        <div class="postList_share">
-                            <div class="postList_share_icon"></div>
-                            <div class="postList_share_val">{{ $post['shares'] }}</div>
+                            <div class="postList_share">
+                                <div class="postList_share_icon"></div>
+                                <div class="postList_share_val">{{ $post['shares'] }}</div>
+                            </div>
                         </div>
                     </div>
 
-
-
+                    <div class="postList_posted">
+                        <div class="postList_posted_author">
+                            <span>Опубликовано администратором</span>
+                        </div>
+                        <div class="postList_posted_date">
+                            <span>{{ $post['created_at']->format("m.d.Y") }}</span>
+                        </div>
+                    </div>
 
                 </div>
-
-
-            </a>
+            </div>
     @endforeach
 </div>
