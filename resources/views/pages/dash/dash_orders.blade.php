0cee04c6b133a07814184785fdadba5db14fc185
tikken <tikken@tikkens-MacBook-Air.local>
2020-06-13 16:47:14 +0300

    test

--- resources/views/pages/dash/dash_orders.blade.php
+++ resources/views/pages/dash/dash_orders.blade.php
@@ -3,15 +3,16 @@
 @section('center')
     <div class="orders">
 
-<? if(empty($orders_actual)) { ?>
+<? if(!empty($orders_actual)) { ?>
         @include('components.orders.orders_actual', ['orders_actual' => $orders_actual, 'user' => $user])
 <? } ?>
 
-        <? if(isset($orders_history)) { ?>
-            @include('components.orders.orders_history',['orders_history' => $orders_history])
-        <? } else { ?>
-            {{--@include('components.orders.orders_empty')--}}
-        <? } ?>
+<? if(isset($orders_history)) { ?>
+    @include('components.orders.orders_history',['orders_history' => $orders_history])
+<? } else { ?>
+    @include('components.orders.orders_empty')
+<? } ?>
+
     </div>
 
 @endsection
