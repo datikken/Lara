538afdc37d9548ac0b150e1e3e1ad800f6e185eb
tikken <tikken@tikkens-MacBook-Air.local>
2020-06-26 22:47:34 +0300

    restore test

--- resources/views/components/main_swiper/main_swiper.blade.php
+++ resources/views/components/main_swiper/main_swiper.blade.php
@@ -1,6 +1,6 @@
 <div class="swiper-container main_slider">
     <div class="swiper-wrapper">
-        <div class="swiper-slide" style="background-image: url('/images/swiper/Slider-image1.png')"></div>
+        <div class="swiper-slide" style="background-image: url('/images/swiper/restore.jpg')"></div>
         <div class="swiper-slide" style="background-image: url('/images/swiper/Slider-image2.png')"></div>
     </div>
     <div class="swiper-pagination"></div>
