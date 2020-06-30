986150fd88ef1444db6cdbd66d270d58a10c9b8f
tikken <tikken@tikkens-MacBook-Air.local>
2020-07-01 02:17:49 +0300

    fixes

--- resources/views/admin/information/editInformation.blade.php
+++ resources/views/admin/information/editInformation.blade.php
@@ -18,12 +18,12 @@
             <input class="uk-input" type="text" name="fizik_title" id="fizik_title" placeholder="Заг физ лица" required>
         </div>
 
-        <div class="uk-margin uk-form-row">
-            <textarea cols="4" rows="4" class="uk-text-area" type="text" name="urik_text" id="urik_text" placeholder="Текст ор лица" required></textarea>
+        <div class="form-group green-border-focus">
+            <textarea class="form-control"  name="urik_text" id="urik_text" rows="5" style="resize: none;" placeholder="Текст для юр лица"></textarea>
         </div>
 
-        <div class="uk-margin uk-form-row">
-            <textarea cols="4" rows="4" class="uk-text-area" type="text" name="fizik_text" id="fizik_text" placeholder="Текст физ лица" required></textarea>
+        <div class="form-group green-border-focus">
+            <textarea class="form-control"  name="fizik_text" id="fizik_text" rows="5" style="resize: none;" placeholder="Текст для физ лица"></textarea>
         </div>
 
         <div class="uk-margin" uk-margin>
