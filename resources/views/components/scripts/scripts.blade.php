61cf60510d580a31f94c96545b9bac1bd698aebf
tikken <tikken@tikkens-MacBook-Air.local>
2020-07-27 23:19:53 +0300

    ck

--- resources/views/components/scripts/scripts.blade.php
+++ resources/views/components/scripts/scripts.blade.php
@@ -2,6 +2,6 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
 <script async="false" src="//api-maps.yandex.ru/2.1/?lang=ru_RU" id="scriptonload-__api_maps_yandex_ru_2_1__lang_ru_RU"></script>
 <script src="https://cdn.jsdelivr.net/npm/uikit@3.4.2/dist/js/uikit.min.js"></script>
-<script src="http://cdn.ckeditor.com/4.6.2/standard-all/ckeditor.js"></script>
+<script src="https://cdn.ckeditor.com/4.6.2/standard-all/ckeditor.js"></script>
 {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.4.2/gsap.min.js"></script>--}}
 <script src="{{ asset('js/app.js') }}" defer></script>
