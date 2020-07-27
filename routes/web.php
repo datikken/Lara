f3c9906234cf2b894bf29f9b261d9d6f14ab2955
tikken <tikken@tikkens-MacBook-Air.local>
2020-07-28 00:07:05 +0300

    fore https

--- routes/web.php
+++ routes/web.php
@@ -177,3 +177,7 @@ Route::post('StoreNewPassword', ['uses' => 'ChangePasswordController@store','as'
 
 //Test email template
 Route::get('/mailtemplate', ['uses' => "SendEmailController@testEmailTemplate", 'as' => 'mailtemplate']);
+
+if (env('APP_ENV') === 'production') {
+    URL::forceSchema('https');
+}
