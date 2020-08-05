96fd215d4039d3b137b2389b6510e01befa91c44
tikken <tikken@tikkens-MacBook-Air.local>
2020-08-06 01:00:21 +0300

    bit changes

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -33,12 +33,12 @@
 <body>
 <div class="admin_wrap" style="overflow: hidden; height: 100vh;">
 
-<nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="/admin">{{$userData['name']}}</a>
+<nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow" style="height: 70px; background-color: #292930!important;">
+    {{--<a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="/admin">{{$userData['name']}}</a>--}}
+    <img class="header_wrapper-item_logo-item ml-3" src="/images/icons/main_logo.png" alt="Logo">
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search" style="margin: 20px 0;">
     <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
             <a class="nav-link" href="{{ route('logout') }}">Выйти</a>
