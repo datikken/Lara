902b0db503efea050611f89bb06d3c63599fc4d3
ftp <krillt2_tikken@bebop.beget.ru>
2020-08-05 22:34:58 +0300

    documents

---
+++ app/Http/Controllers/DocumentsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Documents;
+use Illuminate\Http\Request;
+
+class DocumentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Documents  $documents
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Documents $documents)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Documents  $documents
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Documents $documents)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Documents  $documents
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Documents $documents)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Documents  $documents
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Documents $documents)
+    {
+        //
+    }
+}
