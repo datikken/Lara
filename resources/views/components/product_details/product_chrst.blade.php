f201772b08cda1082058418b0658fa16cd0e6039
tikken <tikken@tikkens-MacBook-Air.local>
2020-08-24 02:43:47 +0300

    characteristic fix

--- resources/views/components/product_details/product_chrst.blade.php
+++ resources/views/components/product_details/product_chrst.blade.php
@@ -5,20 +5,24 @@
 } ?>">
     <div class="prdch_wrap">
 
-
         @foreach($chrst as $key=>$item)
-
-            @if($key != 'STMC')
+            @if($key != 'STMC' && $key != 'Чип')
             <div class="prdch_item">
                 <div class="prdch_name">
-                    <span>{{ $key }}</span>
+                    <span>
+                        <? if($key === 'color') {
+                                echo 'Цвет';
+                            } else {
+                                echo $key;
+                            }
+                        ?>
+                    </span>
                 </div>
                 <div class="prdch_val">
                     <span>{{ $item }}</span>
                 </div>
             </div>
             @endif
-
 
         @endforeach
 
