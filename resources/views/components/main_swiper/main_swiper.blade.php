194ae5176fd763925deaa511d83101ba40441a06
tikken <tikken@tikkens-MacBook-Air.local>
2020-09-03 22:44:41 +0300

    main page fixes

--- resources/views/components/main_swiper/main_swiper.blade.php
+++ resources/views/components/main_swiper/main_swiper.blade.php
@@ -4,8 +4,8 @@
             <div class="swiper-slide" style="background-image: url('{{Storage::disk('local')->url('main_slider_images/' . $slide->image )}}')"></div>
         @endforeach
     </div>
-    <div class="swiper-pagination"></div>
-    <div class="swiper-button-prev"></div>
-    <div class="swiper-button-next"></div>
+    <div class="swiper-pagination main_slider_pagination"></div>
+    <div class="swiper-button-prev main_slider-nav"></div>
+    <div class="swiper-button-next main_slider-nav"></div>
     <div class="swiper-scrollbar"></div>
 </div>
