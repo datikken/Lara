98de72ed873953cd4596a300e9c31a461ddf0231
tikken <tikken@tikkens-MacBook-Air.local>
2020-09-08 22:43:53 +0300

    fixes

--- resources/views/components/product_details/product_chrst.blade.php
+++ resources/views/components/product_details/product_chrst.blade.php
@@ -5,26 +5,71 @@
 } ?>">
     <div class="prdch_wrap">
 
-        @foreach($chrst as $key=>$item)
-            @if($key != 'STMC' && $key != 'Чип')
+        {{--{{  dd($chrst) }}--}}
+
+
             <div class="prdch_item">
                 <div class="prdch_name">
                     <span>
-                        <? if($key === 'color') {
-                                echo 'Цвет';
-                            } else {
-                                echo $key;
-                            }
-                        ?>
+                       Цвет
                     </span>
                 </div>
                 <div class="prdch_val">
-                    <span>{{ $item }}</span>
+                    <span>{{ $chrst->color }}</span>
                 </div>
             </div>
-            @endif
+            <div class="prdch_item">
+                <div class="prdch_name">
+                    <span>
+                       Ресурс страниц
+                    </span>
+                </div>
+                <div class="prdch_val">
+                    <span>{{ $chrst->Ресурс }} текстовых страницы А4</span>
+                </div>
+            </div>
+            <div class="prdch_item">
+                <div class="prdch_name">
+                    <span>
+                       Гарантия
+                    </span>
+                </div>
+                <div class="prdch_val">
+                    <span>12 мес</span>
+                </div>
+            </div>
+            <div class="prdch_item">
+                <div class="prdch_name">
+                    <span>
+                       Размер в упаковке (ШхДхВ)мм
+                    </span>
+                </div>
+                <div class="prdch_val">
+                    <span>{{ $chrst->Ширина }} х {{ $chrst->Длина }} x {{ $chrst->Высота }}</span>
+                </div>
+            </div>
+            <div class="prdch_item">
+                <div class="prdch_name">
+                    <span>
+                       Вес в упаковке (кг.)
+                    </span>
+                </div>
+                <div class="prdch_val">
+                    <span>{{ $chrst->Вес }}</span>
+                </div>
+            </div>
+            <div class="prdch_item">
+                <div class="prdch_name">
+                    <span>
+                       Страна производитель
+                    </span>
+                </div>
+                <div class="prdch_val">
+                    <span>Китай</span>
+                </div>
+            </div>
 
-        @endforeach
+
 
     </div>
 </div>
