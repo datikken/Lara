3b9555ec36f2b80eaf636af7de455b79cfbae7b4
tikken <tikken@tikkens-MacBook-Air.local>
2020-09-13 21:14:11 +0300

    menu fixes

--- resources/js/vue/components/menu/DesktopMainMenuDropDown.vue
+++ resources/js/vue/components/menu/DesktopMainMenuDropDown.vue
@@ -1,6 +1,6 @@
 <template>
     <a href="/" class="menu_wrapper-item_menuText-link">
-        <div class="menu_wrapper-item_menuText">
+        <div class="menu_wrapper-item_menuText" @mouseenter="showDesktopMenuDropDown" @mouseleave="hideDropDown">
             <img
                 src="/images/icons/menu_decor.svg"
                 class="menu_wrapper-item_menuText-decor"
@@ -10,9 +10,9 @@
             <div class="menu_wrapper-item_menuText-header">
                 <span class="menu_wrapper-item_menuText-header_item">МЕНЮ</span>
 
-                <ul @mouseenter="showAllDropDowns"
-                    @mouseleave="hideAllDropDowns"
-                    class="menu_wrapper-item-main_menu-item-text_secondary secondaryBottom as-none" data-informationTriggerMenu>
+                <ul @mouseleave="hideAllDropDowns"
+                    class="menu_wrapper-item-main_menu-item-text_secondary secondaryBottom"
+                    data-informationMenuDropdown>
 
                     <li class="menu_wrapper-item-main_menu-item-text_secondary-text" v-for="post in informationPosts" :key="post.id">
                         <a href="/information">
@@ -21,7 +21,7 @@
                     </li>
                 </ul>
 
-                <ul class="menu_wrapper-item-main_menu as-none" data-desktopMenuBlock @mouseleave="hideDropDown">
+                <ul class="menu_wrapper-item-main_menu" data-desktopMenuBlock @mouseenter="showDesktopMenuDropDown"  @mouseleave="hideDropDown">
                     <li class="menu_wrapper-item-main_menu-item">
                         <span class="menu_wrapper-item-main_menu-item-text">
                             <a href="/catalog">Каталог</a>
@@ -52,7 +52,7 @@
                             <!--<img src="/images/icons/arrow_right.svg" alt="arrow"/>-->
                         </span>
                     </li>
-                    <li @mouseenter="showDropDown"
+                    <li @mouseenter="showInformationDropDown"
                         class="menu_wrapper-item-main_menu-item"
                     >
                         <span class="menu_wrapper-item-main_menu-item-text">
@@ -90,34 +90,57 @@
                 'GET_ALL_INFORMATION_POSTS'
             ]),
             hideAllDropDowns() {
-                let informationMenu = this.$el.querySelector('[data-informationTriggerMenu]');
+                let informationMenu = this.$el.querySelector('[data-informationMenuDropdown]');
                 let desMenu = this.$el.querySelector('[data-desktopMenuBlock]');
 
-                informationMenu.classList.add('as-none');
+                informationMenu.classList.remove('showInformationMenuDropdown');
                 desMenu.classList.add('as-none');
             },
-            showAllDropDowns() {
-                let informationMenu = this.$el.querySelector('[data-informationTriggerMenu]');
-                let desMenu = this.$el.querySelector('[data-desktopMenuBlock]');
-
-                    informationMenu.classList.remove('as-none');
-                    desMenu.classList.remove('as-none');
-            },
             hideDropDown() {
-                let menu = this.$el.querySelector('[data-informationTriggerMenu]');
+                let menu = this.$el.querySelector('[data-informationMenuDropdown]');
                 let dropdown = this.$el.querySelector('[data-desktopMenuBlock]');
 
-                    dropdown.classList.add('as-none');
-                    menu.classList.add('as-none');
+                dropdown.classList.add('as-none');
+                menu.classList.remove('showInformationMenuDropdown');
+                dropdown.classList.remove('showDesktopMenuDropdown');
             },
-            showDropDown() {
-                let informationMenu = this.$el.querySelector('[data-informationTriggerMenu]');
-                    informationMenu.classList.remove('as-none');
+            showAllDropDowns() {
+                let informationMenu = this.$el.querySelector('[data-informationMenuDropdown]');
+                let desMenu = this.$el.querySelector('[data-desktopMenuBlock]');
+
+                desMenu.classList.remove('as-none');
+                desMenu.classList.add('showDesktopMenuDropdown');
+
+                informationMenu.classList.remove('as-none');
+                informationMenu.classList.remove('showInformationMenuDropdown');
+            },
+            showInformationDropDown() {
+                let informationMenu = this.$el.querySelector('[data-informationMenuDropdown]');
+                informationMenu.classList.add('showInformationMenuDropdown');
+                informationMenu.classList.remove('as-none');
+            },
+            showDesktopMenuDropDown() {
+                let dropdown = this.$el.querySelector('[data-desktopMenuBlock]');
+                    dropdown.classList.add('showDesktopMenuDropdown');
             }
         }
     }
 </script>
 
 <style scoped>
-
+[data-desktopMenuBlock] {
+    height: 0;
+    transition: height .2s;
+}
+.showDesktopMenuDropdown {
+    height: 280px;
+}
+[data-informationMenuDropdown] {
+    height:0;
+    overflow: hidden;
+}
+.showInformationMenuDropdown {
+    right: -14.2rem;
+    height:unset;
+}
 </style>
