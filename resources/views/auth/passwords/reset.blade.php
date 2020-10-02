b45d7e2a9fbb73c23fd6acadf653b598024208bf
tikken <tikken@tikkens-MacBook-Air.local>
2020-10-02 07:20:32 +0300

    test

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.index')
 
-@section('content')
+@section('center')
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
