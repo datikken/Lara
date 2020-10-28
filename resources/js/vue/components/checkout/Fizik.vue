1f45957876364b1b30af646a7f4ae4be8dad9a2f
tikken <tikken23@gmail.com>
2020-10-29 02:05:25 +0300

    test

--- resources/js/vue/components/checkout/Fizik.vue
+++ resources/js/vue/components/checkout/Fizik.vue
@@ -139,6 +139,9 @@
                         data.tel = phone;
 
                         this.REFRESH_CUTOMER_DATA(data);
+
+
+                        console.warn('saveDataToStorage', data)
                 }
             },
             fillInputsData() {
