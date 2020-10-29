1a0139269e122a8e4d61fb8c7e241e93c515d43f
tikken <tikken23@gmail.com>
2020-10-29 05:33:28 +0300

    reverted

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,23 +6,13 @@ import './components/Swiper'
 import Checkbox from './components/Checkbox'
 import Validator from './functions/validator'
 import SearchController from './modules/SearchController'
-
-//main page
 import OwlCarouselController from './modules/OwlCarouselController'
-//catalog page
 import Catalog from './modules/Catalog'
-//information
 import InformationController from './modules/InformationController'
-//hits
-
-//blog
 import BlogController from './modules/BlogController'
 import BlogPostController from './modules/BlogPostController'
-
-//contacts
 import ContactsFormController from './modules/ContactsFormController'
 import SimpleMap from './components/Map';
-
 import CartController from './modules/cart/CartController'
 import ProductDetailsSliderController from './modules/ProductDetailsSliderController'
 import ProductFeedbackController from './modules/ProductFeedbackController'
@@ -39,78 +29,35 @@ import DeliveryFormController from './modules/DeliveryFormController'
 import LandingController from './modules/LandingController'
 import DadataController from './modules/DadataController'
 
-
 $.ajaxSetup({
     headers: {
         'X-CSRF-TOKEN': window.token
     }
 });
 
-$(document).ready(function() {
-    //every page
+$(document).ready(function () {
     new Checkbox();
     new Validator();
     new SearchController();
-
-    let path = window.location.pathname;
-
-    //main page
-    if(path === '/') {
-        new OwlCarouselController();
-    }
-
-    if(path === '/catalog') {
-        new Catalog();
-    }
-
-    if(path === '/information') {
-        new InformationController();
-    }
-
-    if(path === '/blog') {
-        new BlogController();
-        new BlogPostController();
-    }
-
-    if(path === '/contacts') {
-        new ContactsFormController();
-        new SimpleMap();
-    }
-
-    if(path === '/about') {
-        new AboutController();
-    }
-
-    if(path.indexOf('product') >= 0) {
-        new ProductDetailsSliderController();
-        new OrdersHistoryController();
-        new ProductFeedbackController();
-        new ProductDetailsMenuController();
-    }
-
-    if(path === '/dash') {
-        new DashController();
-        new ProfileController();
-    }
-
-    if(path === '/cart') {
-        new CartCheckoutVueApp();
-        new CartController();
-    }
-
-    if(path === '/login') {
-        new RegisterController();
-    }
-
-    if(path === '/tracking') {
-        new TrackingFeedController();
-    }
-
-    // new DadataController();
-    // new LandingController();
-
-    //information page
-    // new DeliveryFormController();
-
-
+    new OwlCarouselController();
+    new Catalog();
+    new InformationController();
+    new BlogController();
+    new BlogPostController();
+    new ContactsFormController();
+    new SimpleMap();
+    new AboutController();
+    new ProductDetailsSliderController();
+    new OrdersHistoryController();
+    new ProductFeedbackController();
+    new ProductDetailsMenuController();
+    new DashController();
+    new ProfileController();
+    new CartCheckoutVueApp();
+    new CartController();
+    new RegisterController();
+    new TrackingFeedController();
+    new DadataController();
+    new LandingController();
+    new DeliveryFormController();
 });
--- resources/views/components/scripts/scripts.blade.php
+++ resources/views/components/scripts/scripts.blade.php
@@ -8,6 +8,7 @@
 
 <script src="https://api-maps.yandex.ru/2.1/?apikey=6a0c633c-193e-4f23-9ff1-01e421de70a5&lang=ru_RU" type="text/javascript"></script>
 <script src="https://cdn.jsdelivr.net/npm/uikit@3.4.2/dist/js/uikit.min.js"></script>
+
 <script src="https://widget.unitpay.ru/unitpay.js"></script>
 
 <script src="{{ asset('js/app.js') }}" async defer></script>
